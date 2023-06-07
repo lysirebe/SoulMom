651e772fa0d89f670deaf1bae444ba77c576d037
lysirebe <lysirebe@gmail.com>
2023-06-07 18:10:31 +0100

    completed all CRUD routes, fixed userid issue

--- backend/controllers/journal.js
+++ backend/controllers/journal.js
@@ -1,25 +1,82 @@
-// const pool = require("../config/db");
+const pool = require("../config/db");
 
-// module.exports = (req, res) => {
+const getEntries = (req, res) => {
 
-//   const { title, entry } = req.body;
-//   const userid = req.user.user_id;
+  userid = req.user.user_id;
+  pool.query("SELECT * FROM journal WHERE user_id = $1", [userid], (err, result) => {
+    if (err) {
+      console.error(err.message)
+      res.status(500).json({ message: err.message })
+    } else {
+      res.status(200).json(result.rows)
+    }
+  })
+}
 
-//   pool.query(
-//     "INSERT INTO journal (user_id, title, entry) VALUES ($1, $2, $3) RETURNING *",
-//     [userid, title, entry],
-//     (err, result) => {
+const createEntry = (req, res) => {
 
-//       if (err) {
-//         console.error(err.message)
-//         res.status(500).json({ message: "Cant add entry to db" })
+  const { title, entry } = req.body;
 
-//       } else {
-//         res.status(200).json({
-//           message: "entry added to db",
-//           entry: result.rows[0]
-//         })
-//       }
-//   })
-// }
+  const userid = req.user.user_id;
 
+  pool.query(
+    "INSERT INTO journal (user_id, title, entry) VALUES ($1, $2, $3) RETURNING *",
+    [userid, title, entry],
+    (err, result) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).json({ message: "Cant add entry to db" });
+      } else {
+        res.status(200).json({
+          message: "entry added to db",
+          entry: result.rows[0],
+        });
+      }
+    }
+  );
+}
+
+const updateEntry = (req, res) => {
+  // const userid = req.user.user_id;
+  const {journalid} = req.params;
+  const { entry, title } = req.body;
+
+  pool.query("UPDATE journal SET entry = $1, title = $2 WHERE journal_id = $3 RETURNING *", [entry, title, journalid], (err, result) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ message: "Cant update journal entry" });
+    } else {
+      res.status(200).json({
+        message: "entry updated",
+        entry: result.rows[0]
+      })
+    }
+  });
+}
+
+const deleteEntry = (req, res) => {
+  try {
+    const { journalid } = req.params;
+    const query = "DELETE FROM journal WHERE journal_id = $1"
+    const values = [journalid]
+    const result = pool.query(query, values)
+    
+      if (result.rowCount === 0) {
+        res.status(404).json({message:"Entry not found"})
+      } else {
+        res.status(200).json({message:"entry deleted"})
+    }
+    
+  } catch (err) {
+      console.error(err.message)
+      res.status(400).json({message:err.message})
+    }
+}
+
+// have a recently deleted file 
+module.exports = {
+  getEntries,
+  createEntry,
+  updateEntry,
+  deleteEntry
+}
--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,12 +1,11 @@
-const pool = require('../config/db')
+// const pool = require('../config/db')
 const router = require('express').Router()
 const authorise = require('../middleware/authorise')
-// const newuser = require('../controllers/journal')
+const journal = require('../controllers/journal')
 
 
 router.post("/", authorise, async (req, res) => {
   try {
-    
     // after passing middleware req.user now stores all the user data 
     const user = req.user
     res.json(user)
@@ -18,46 +17,51 @@ router.post("/", authorise, async (req, res) => {
   }
 )
 
-
-router.get("/entryList", authorise, async (req, res) => {
-  try {
-    userid = req.user.user_id;
-
-    const entries = await pool.query("SELECT * FROM journal WHERE user_id = $1", [userid])
-    
-    if (!entries) {
-      return res.status(400).json({message: "no entries found"})
-    } else {
-      res.json(entries.rows)
-    }
-  } catch (err) {
-    console.error(err.message)
-    res.status(500).json({message:"cant get entries"})
-  }
-})
-
-router.post("/newEntry", authorise, async (req, res) => {
-  try {
-
-    const userid = req.user.user_id;
-    const { title, entry } = req.body;
-
-    console.log(req.body);
-
-   const newEntry = await pool.query(
-     "INSERT INTO journal (user_id, title, entry) VALUES ($1, $2, $3) RETURNING *",
-     [userid,title,entry]
-   );
-
-    console.log(newEntry.rows[0])
-    res.json(newEntry.rows[0])
-    
-  } catch(err){
-    console.error(err.message)
-    res.status(500).json({message:"Cant add entry to db"})
-}})
-
-
-// router.post("/newEntry", authorise, newuser)
+router.get("/entries", authorise, journal.getEntries);
+router.post("/newEntry", authorise, journal.createEntry);
+router.put("/updateEntry/:journalid", journal.updateEntry);
+router.delete("/deleteEntry/:journalid", journal.deleteEntry);
 
 module.exports = router;
+
+
+// router.get("/entryList", authorise, async (req, res) => {
+//   try {
+    // userid = req.user.user_id;
+
+    // const entries = await pool.query("SELECT * FROM journal WHERE user_id = $1", [userid])
+    
+//     if (!entries) {
+//       return res.status(400).json({message: "no entries found"})
+//     } else {
+//       res.json(entries.rows)
+//     }
+//   } catch (err) {
+//     console.error(err.message)
+//     res.status(500).json({message:"cant get entries"})
+//   }
+// })
+
+// router.post("/newEntry", authorise, async (req, res) => {
+//   try {
+
+//     const userid = req.user.user_id;
+//     const { title, entry } = req.body;
+
+//     console.log(req.body);
+
+//    const newEntry = await pool.query(
+//      "INSERT INTO journal (user_id, title, entry) VALUES ($1, $2, $3) RETURNING *",
+//      [userid,title,entry]
+//    );
+
+//     console.log(newEntry.rows[0])
+//     res.json(newEntry.rows[0])
+    
+//   } catch(err){
+//     console.error(err.message)
+//     res.status(500).json({message:"Cant add entry to db"})
+// }})
+
+
+
